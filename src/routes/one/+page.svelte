bc6d9daf2774ec8c9daed28bea65ce35d964a986
bodav <bodavidsen@gmail.com>
2023-12-10 16:54:22 +0100

    startover

--- src/routes/one/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1>One</h1>
- 
-<p>Some example content</p>
-
-<section class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300">
-    <h1 class="pt-10 text-7xl">About page</h1>
-    <div class="font-serif pt-12 text-2xl">Ciao!</div>
-    </section>
--- src/routes/three/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1>3</h1>
- 
-<p>Some example content</p>
