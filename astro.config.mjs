f05eb910ccd8f112a14ed8abaa0b60395618e15e
bodav <bodavidsen@gmail.com>
2024-01-03 09:59:09 +0100

    added alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   site: 'https://bodav.github.io',
-  base: '/startpage',
+  base: '/startpage'
 });
