806a44f4725a910d751082b1865993cb9b5afe3e
bodav <bodavidsen@gmail.com>
2025-06-15 21:05:16 +0200

    icon cleanup

--- src/components/LinkIcon.tsx
+++ src/components/LinkIcon.tsx
@@ -3,28 +3,28 @@ import GithubIcon from "@/assets/icons/github.svg?react";
 import RedditIcon from "@/assets/icons/reddit.svg?react";
 import JvIcon from "@/assets/icons/jv.svg?react";
 import YcombinatorIcon from "@/assets/icons/ycombinator.svg?react";
-import AccuweatherIcon from "@/assets/icons/accuweather.svg?react";
+import DmiIcon from "@/assets/icons/dmi.svg?react";
 import FacebookIcon from "@/assets/icons/facebook.svg?react";
 import YoutubeIcon from "@/assets/icons/youtube.svg?react";
 import DrIcon from "@/assets/icons/dr.svg?react";
 import XIcon from "@/assets/icons/x.svg?react";
 import StravaIcon from "@/assets/icons/strava.svg?react";
 import PolarFlowIcon from "@/assets/icons/polarflow.svg?react";
-import PusherIcon from "@/assets/icons/pusher.svg?react";
+import PushoverIcon from "@/assets/icons/pushover.svg?react";
 
 const ICONS: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
   github: GithubIcon,
   reddit: RedditIcon,
   jv: JvIcon,
   ycombinator: YcombinatorIcon,
-  accuweather: AccuweatherIcon,
+  dmi: DmiIcon,
   facebook: FacebookIcon,
   youtube: YoutubeIcon,
   dr: DrIcon,
   x: XIcon,
   strava: StravaIcon,
   polarflow: PolarFlowIcon,
-  pusher: PusherIcon
+  pushover: PushoverIcon
 };
 
 interface LinkIconProps {
