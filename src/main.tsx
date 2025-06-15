0475ef8db2ac8b89b3ad659af168be2cfbd9f8b3
bodav <bodavidsen@gmail.com>
2025-06-15 21:13:38 +0200

    novfont embed

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "@/assets/fonts/Geist-Regular.woff2";
-import "@/assets/fonts/Geist-Light.woff2";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
